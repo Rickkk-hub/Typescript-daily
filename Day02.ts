8caaab00a4e54b54345f211d84ff869f508eaeaf
Rickkk-hub <picardal.ricky.roque@gmail.com>
2025-10-15 11:19:02 +0800

    Create Day02.ts

---
+++ Day02.ts
@@ -0,0 +1,14 @@
+//Day02 this a simple Primitive types (string, number, boolean.
+
+let Person: string = "Ricky"
+let Age  = 21;
+let IsAdmin: boolean = true;
+
+
+
+function info(Person: string):string{
+
+    return(Person)
+}
+console.log(`Hello Im ${Person} im ${Age} years old it is ${IsAdmin}?`)
+
