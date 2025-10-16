183588b6edf3ccb4bbc7ccaa87a1692c191d0960
Rickkk-hub <picardal.ricky.roque@gmail.com>
2025-10-16 13:50:13 +0800

    Create Day03.ts

---
+++ Day03.ts
@@ -0,0 +1,13 @@
+ //Type inference
+
+ let a = 15;
+ let b = 20;
+
+
+
+ function add(x:number, y:number ): number
+ {
+   return x + y;
+ }
+ let Sum = add(a, b);
+ console.log(`${a} + ${b} = ${Sum}`)
