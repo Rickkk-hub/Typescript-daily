99f688e4e2edfcec2eb0647ddfefc83884d832fe
Rickkk-hub <picardal.ricky.roque@gmail.com>
2025-10-18 14:48:02 +0800

    Create Day04.ts

---
+++ Day04.ts
@@ -0,0 +1,7 @@
+//	Variables and type annotations.
+let num:number[]= [1,2,3,4,5];
+let fruit:string[] = ["apple", "mango", "orange"];
+
+
+console.log(`numbers: ${num.join(", ",)}`);
+console.log(`fruits: ${fruit.join(", ",)}`);
